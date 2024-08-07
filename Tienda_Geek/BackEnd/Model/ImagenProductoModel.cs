6ba85b03a78efd012cda51c868dbb70728ab9d23
FerUwu19 <114791752+FerUwu19@users.noreply.github.com>
2024-08-06 23:08:03 -0600

    delete tienda vieja

--- Tienda_Geek/BackEnd/Model/ImagenProductoModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BackEnd.Model
-{
-    public class ImagenProductoModel
-    {
-        public int CodigoImagen { get; set; }
-        public int CodigoProducto { get; set; }
-        public string RutaImagen { get; set; }
-    }
-}
